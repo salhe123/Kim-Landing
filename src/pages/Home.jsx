ffcd40c0bcf2dc76b2cf26b3bb86d7d88a56b4ad
Samuel Tiruneh <139230421+Samuel-Tiruneh@users.noreply.github.com>
2025-05-25 09:12:29 +0300

    latest

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import About from '../components/About'
 import Services from '../components/Services'
 import Projects from '../components/Projects'
-import OurTeam from '../components/OurTeam'
+
 import ChatIcon from '../components/ChatIcon'
 import Footer from '../components/Footer'
 export const Home = () => {
@@ -11,9 +12,10 @@ export const Home = () => {
     <>
       <Header />
       <Hero/>
+      <About/>
        <Services/>
        <Projects />
-       <OurTeam />
+     
        <ChatIcon />
        <Footer/>
 
